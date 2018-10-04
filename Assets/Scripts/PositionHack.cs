8ef73d4f1a3f247e2161a6597ac0a2bcfe4245b2
Aaron Oostdijk <aoostdijk@gmail.com>
2018-10-04 11:39:36 +0200

    Added ability for mouse-manipulation when not having VR enabled.

--- Assets/Scripts/PositionHack.cs
+++ Assets/Scripts/PositionHack.cs
@@ -9,4 +9,27 @@ public class PositionHack : MonoBehaviour {
         //UnityEngine.XR.InputTracking.disablePositionalTracking = true;
        // transform.position = Vector3.zero;
     }
+	
+#if UNITY_EDITOR
+    static GameObject sceneCam;
+    Camera cam;
+
+    void OnDrawGizmos() {
+        if (!UnityEditor.PlayerSettings.virtualRealitySupported)
+        {
+            if (sceneCam == null)
+            {
+                sceneCam = GameObject.Find("SceneCamera");
+            }
+            if (cam == null)
+            {
+                cam = GetComponent<Camera>();
+            }
+            if (cam && sceneCam)
+            {
+                cam.transform.rotation = sceneCam.transform.rotation;
+            }
+        }
+    }
+#endif
 }
