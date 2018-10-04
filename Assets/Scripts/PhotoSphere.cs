c2e9af6007daab550afbec782f770d2852abad7b
Aaron Oostdijk <aoostdijk@gmail.com>
2018-10-04 17:11:44 +0200

    Added ability to use webcams on PhotoSpheres

--- Assets/Scripts/PhotoSphere.cs
+++ Assets/Scripts/PhotoSphere.cs
@@ -1,29 +1,54 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PhotoSphere : MonoBehaviour {
-	MeshRenderer rend;
-    
-    Transform _t;
+	
+	[ContextMenu("GetWebcames")]
+	void GetWebcams() {
+		webCamNames = new string[WebCamTexture.devices.Length];
+		int index = 0;
+		foreach( WebCamDevice d in WebCamTexture.devices ) {
+			webCamNames[index++] = d.name;
+		}
+	}
 
-    // Use this for initialization
-    void Awake () {
+	public string[] webCamNames = {};
+	public bool useWebCam = false;
+	public string webCamName = "";
+	
+	MeshRenderer rend;
+	WebCamTexture camTex;
+	Transform _t;
+
+	// Use this for initialization
+	void Awake () {
 		rend = GetComponent<MeshRenderer>();
 		Hide();
-        _t = transform;
-
-    }
+		_t = transform;
+	}
 	
 	public void Show() {
+		if ( useWebCam && camTex == null ) {
+			camTex = new WebCamTexture(webCamName);
+			rend.material.shader = Shader.Find("UI/Default");
+		}
+
+		rend.material.mainTexture = camTex;
+		camTex.Play();
+		
 		rend.enabled = true;
 	}
 
 	public void Hide() {
 		rend.enabled = false;
+
+		if ( camTex != null ) {
+			camTex.Stop();
+		}
 	}
 
-    void Update()
+	void Update()
     {
         //TODO: optimize
         _t.position = Camera.main.transform.position;
